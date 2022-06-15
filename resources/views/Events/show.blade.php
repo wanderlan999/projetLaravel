2089ca1cc7144849e851729d66aea0d750f00097
wanderlan <wanderlanribeirodasilva51@gmail.com>
2022-06-15 13:15:37 -0300

    corrigindo layout eventos

--- resources/views/Events/show.blade.php
+++ resources/views/Events/show.blade.php
@@ -6,10 +6,10 @@
 
 <div class="col-md-10 offset-md-1">
     <div class="row">
-        <div id="image-container" class="cold-md-6">
+        <div id="image-container" class="col-md-6">
             <img src="/img/events/{{$event->image}}" class="img-fluid" alt="{{$event->title}}">
             </div>
-        <div id="info-container" class="cold-md-6">
+        <div id="info-container" class="col-md-6">
          <h1>{{$event->title}}</h1>
            <p class="event-city"><ion-icon name="location-outline"></ion-icon>{{$event->city}}</p>
               <p class="events-participants"><ion-icon name="people-outline"></ion-icon>x participantes</p>
